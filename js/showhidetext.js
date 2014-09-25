2a91b88c6f65089e366e9ad6efa238b65acfd191
cdracars <cdracars@gmail.com>
2014-09-25 15:32:37 -0500

    Add the gulp

--- js/showhidetext.js
+++ js/showhidetext.js
@@ -4,20 +4,22 @@
  */
 
 // JavaScript should be made compatible with libraries other than jQuery by
-// wrapping it with an "anonymous closure". See:
+// wrapping it with an 'anonymous closure'. See:
 // - http://drupal.org/node/1446420
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function ($) {
+  'use strict';
   Drupal.behaviors.showhidetext = {
-    attach: function (context, settings) {
-      $(".listing-text").addClass('is-hidden');
-      $(".listing .views-row").hover(
+    attach: function (context) {
+      $('.listing-text', context).addClass('is-hidden');
+      $('.listing .views-row', context).hover(
         function() {
-          $(this).find(".listing-text").addClass('is-shown');
+          $(this).find('.listing-text').addClass('is-shown');
         }, function() {
-          $(this).find(".listing-text").addClass('is-hidden').removeClass('is-shown');
+          $(this).find('.listing-text').addClass('is-hidden')
+                 .removeClass('is-shown');
         }
       );
     }
-  }
+  };
 })(jQuery);
